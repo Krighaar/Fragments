f66325eab7a6760b244a738907176103e0181edf
Peter K <millipeter15@hotmail.com>
2015-03-07 08:57:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/peter/fragments/MainFragment.java
@@ -0,0 +1,98 @@
+package com.example.peter.fragments;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MainFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MainFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainFragment extends SuperFragment implements View.OnClickListener {
+
+    private static final String TAG = "PlaceHolderFragment";
+
+    private OnFragmentInteractionListener mListener;
+    private MainActivity ma;
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, true);
+        int[] clickButtons = new int[]{R.id.launch
+        };
+        for (int i : clickButtons) {
+            rootView.findViewById(i).setOnClickListener(this);
+        }
+        ma = (MainActivity) getActivity();
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.launch:
+                Toast.makeText(ma, "Button cliked, map should open", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    protected boolean canGoBack() {
+        return false;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
